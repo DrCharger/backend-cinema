352f00287a8d7668f7db3b2268312f4987d7de19
Dr.Charger <dmitriy.nasa@gmail.com>
2023-05-17 15:55:19 +0300

    0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 const port = process.env.PORT || 4200
-const host = process.env.HOST || 'localhost'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 	app.enableCors()
-	await app.listen(port, host)
+	await app.listen(port, '0.0.0.0')
 }
 bootstrap()
